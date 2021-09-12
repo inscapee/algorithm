d7b6edfca1584187fe28b2479250bb2521c93352
fine <dlgmltjr0925@gmail.com>
2021-09-12 22:36:52 +0900

    prettier 적용, 주제 생성 프로젝트 구현

---
+++ scripts/make_subject.js
@@ -0,0 +1,22 @@
+const fs = require('fs');
+
+// 참가자
+const participants = ['bright', 'luka', 'negan', 'pepe', 'kei'];
+
+const directoryName = process.argv[2];
+
+if (!directoryName) {
+  return console.log('주제 명을 입력해주세요.(210915_subject');
+}
+
+if (!fs.existsSync(directoryName)) fs.mkdirSync(directoryName);
+
+for (const name of participants) {
+  const participantPath = `${directoryName}/${name}`;
+  const readmePath = `${participantPath}/README.md`;
+  if (!fs.existsSync(participantPath)) fs.mkdirSync(participantPath);
+  if (!fs.existsSync(readmePath))
+    fs.writeFile(readmePath, '', (err) => {
+      if (err) console.error(err);
+    });
+}

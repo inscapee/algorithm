b24f6919a3f9e8dea62a0aa1a8f95b12270ebaf0
negan <negan@insterior.co.kr>
2021-09-23 17:09:53 +0900

    1주차 과제 수정

--- 210923_implementation/negan/10808.js
+++ 210923_implementation/negan/10808.js
@@ -1,11 +1,13 @@
 const input = require('fs').readFileSync('/dev/stdin').toString();
 const result = Array.from({length: 26}, () => 0);
 
-for(let i; i = input.length; i++) {
-    const index = input[i].charCodeAt() - 97;
-    result[index]++;
+const alphabetNum = (input) => {
+    for(let i = 0; i < input.length; i++) {
+        const index = input[i].charCodeAt() - 97;
+        result[index]++;
+    }
+
+    console.log(result.join(' '));
 }
 
-console.log(result.join(' '));
-
-// 런타임 에러 (TypeError) <- 발생
+alphabetNum(input)

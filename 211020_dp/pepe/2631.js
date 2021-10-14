d980a8ea6091b6f36cfa7da3858a6e6a6f27ae34
Minjeong kang <snowdrop1202@gmail.com>
2021-10-14 17:24:07 +0900

    feat: 2631 줄세우기 스크립트 추가

---
+++ 211020_dp/pepe/2631.js
@@ -0,0 +1,22 @@
+/* 줄세우기 */
+
+const input = `7
+3
+7
+5
+2
+6
+1
+8
+4`.split('\n');
+
+const quickSort = arr => {
+  const [max, ...children] = arr;
+  const standard = Math.ceil(max / 2);
+  let move = 0;
+
+  
+
+}
+
+quickSort(input)

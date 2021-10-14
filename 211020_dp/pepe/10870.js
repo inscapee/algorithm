9197743d1b8fd186ddd6c917f7999fa6ae796c82
Minjeong kang <snowdrop1202@gmail.com>
2021-10-14 15:39:10 +0900

    fix: 10870 파라미터 0, 또는 1일 경우 대응
    * 인자값에 0 또는 1이 들어왔을 경우 대응이 없었으므로 result를 배열로 변경 후 result[input] 으로 출력

--- 211020_dp/pepe/10870.js
+++ 211020_dp/pepe/10870.js
@@ -3,15 +3,15 @@
 // const input = 17;
 const input = require('fs').readFileSync('/dev/stdin').toString();
 
-const fibonacci = input => {
+const fibonacci = index => {
   let i = 2;
   let prev = 0;
   let current = 1;
-  let result = prev + current;
-  while (i <= input) {
-    result = prev + current;
+  let result = [prev, current];
+  while (i <= index) {
+    result = [...result, prev + current];
     prev = current;
-    current = result;
+    current = result[i];
     i++;
   }
 

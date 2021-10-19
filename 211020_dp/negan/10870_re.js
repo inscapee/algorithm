9e5a61cff4068e19452bcb7b3ffa8b7968923cf0
negan <negan@insterior.co.kr>
2021-10-19 14:58:06 +0900

    10870 피보나치수 실패

---
+++ 211020_dp/negan/10870_re.js
@@ -0,0 +1,19 @@
+function solution(input) {
+  const arr = Array.from({ length: input }, () => 0);
+  if (input === 0) {
+    return console.log(0);
+  }
+
+  function dp(x) {
+    if (x === 1) return (arr[x] = 1);
+    if (x === 2) return (arr[x] = 1), (arr[x - 1] = 1);
+    if (arr[x - 1] !== 0) return arr[x];
+    return (arr[x] = dp(x - 1) + dp(x - 2));
+  }
+
+  dp(input);
+  console.log(arr[input]);
+  console.log(arr);
+}
+
+solution(input);
